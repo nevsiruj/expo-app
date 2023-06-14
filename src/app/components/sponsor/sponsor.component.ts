1183d4b07edd767ed06690c7d51f5e6d7950c76d
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-06-13 22:58:48 -0300

    1 file modified, 2 added

---
+++ src/app/components/sponsor/sponsor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor',
+  templateUrl: './sponsor.component.html',
+  styleUrls: ['./sponsor.component.css'],
+})
+export class SponsorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
